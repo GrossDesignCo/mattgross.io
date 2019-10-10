3c92b3809801e60acad83e2a7062ac4a90caaa7d
Matt Gross <magross@tesla.com>
2019-10-09 18:54:21 -0700

    misc updates

--- src/components/Website/Footer/index.js
+++ src/components/Website/Footer/index.js
@@ -7,9 +7,13 @@ function Footer() {
     <footer className="nowrap sticky">
       <div className="cap-width">
         <div>
-          <a href="https://www.linkedin.com/in/m-gross/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/m-gross/" target="_blank">
+            LinkedIn
+          </a>
           {", "}
-          <a href="https://github.com/GrossDesignCo">GitHub</a>
+          <a href="https://github.com/GrossDesignCo" target="_blank">
+            GitHub
+          </a>
         </div>
         <div>© {new Date().getFullYear()}</div>
       </div>
