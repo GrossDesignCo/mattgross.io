0f027ca7536e5b445dd85b8ff968e3fde3cfa758
Matt Gross <mattgrosswork@gmail.com>
2024-05-28 17:43:15 -0700

    fade in exploration 1

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 
 import '../styles/vars.css';
-import '../styles/animation.css';
 import '../styles/general.css';
 
 function MyApp({ Component, pageProps }) {
