23a7f4644002ac5ec54862b8028dd5aaed9d395a
Matt Gross <mattgrosswork@gmail.com>
2024-07-18 13:34:35 -0700

    add microtransition to playpause

--- components/icons/play-pause.jsx
+++ components/icons/play-pause.jsx
@@ -7,29 +7,28 @@ export const IconPlayPause = ({ paused }) => {
       viewBox="0 0 21 21"
       xmlns="http://www.w3.org/2000/svg"
     >
-      {paused ? (
-        <polygon
-          points="4,2 18,10.5 4,19"
-          strokeLinejoin="round"
-          className="shape"
-          vector-effect="non-scaling-stroke"
-        />
-      ) : (
-        <>
-          <polygon
-            points="3,2 8,2 8,19 3,19"
-            strokeLinejoin="round"
-            className="shape"
-            vector-effect="non-scaling-stroke"
-          />
-          <polygon
-            points="18,2 18,19 13,19 13,2"
-            strokeLinejoin="round"
-            className="shape"
-            vector-effect="non-scaling-stroke"
-          />
-        </>
-      )}
+      {/* Play */}
+      <polygon
+        points="4,2 18,10.5 4,19"
+        strokeLinejoin="round"
+        className="shape play"
+        vector-effect="non-scaling-stroke"
+      />
+
+      {/* Pause */}
+      <polygon
+        points="3,2 8,2 8,19 3,19"
+        strokeLinejoin="round"
+        className="shape pause"
+        vector-effect="non-scaling-stroke"
+      />
+      <polygon
+        points="18,2 18,19 13,19 13,2"
+        strokeLinejoin="round"
+        className="shape pause"
+        vector-effect="non-scaling-stroke"
+      />
+
       {/* TODO: Animate the play/pause transition, check out SMIL, js, maybe mini-canvas lol */}
     </svg>
   );
